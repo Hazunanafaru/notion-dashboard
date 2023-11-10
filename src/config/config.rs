743b35cb12fabda09baca2ee2373efc101a7832a
husni.zuhdi@accelbyte.net <husninaufalz@gmail.com>
2023-11-10 23:18:00 +0700

    chore: add unit test for Config

--- src/config/config.rs
+++ src/config/config.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 /// Struct Config for setup environment variables
+#[derive(PartialEq, Debug)]
 pub struct Config {
     pub postgres_url: String,
     pub postgres_port: String,
@@ -14,11 +15,11 @@ pub struct Config {
 
 impl Default for Config {
     fn default() -> Self {
-        let postgres_url: String = "".to_string();
-        let postgres_port: String = "".to_string();
-        let postgres_user: String = "".to_string();
+        let postgres_url: String = "localhost".to_string();
+        let postgres_port: String = "5432".to_string();
+        let postgres_user: String = "user".to_string();
         let postgres_password: String = "".to_string();
-        let postgres_db: String = "".to_string();
+        let postgres_db: String = "notion_dashboard".to_string();
         let notion_token: String = "".to_string();
         let notion_dj_database_id: String = "".to_string();
         let notion_dj_dashboard_h1_id: String = "".to_string();
@@ -65,5 +66,65 @@ impl Config {
             notion_dj_database_id,
             notion_dj_dashboard_h1_id,
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_default() {
+        let postgresql_url = "localhost".to_string();
+        let postgresql_port = "5432".to_string();
+        let postgresql_user = "user".to_string();
+        let postgresql_password = "".to_string();
+        let postgresql_db = "notion_dashboard".to_string();
+        let notion_token = "".to_string();
+        let notion_dj_database_id = "".to_string();
+        let notion_dj_dashboard_h1_id = "".to_string();
+
+        let result = Config::default();
+
+        assert_eq!(result.postgres_url, postgresql_url);
+        assert_eq!(result.postgres_port, postgresql_port);
+        assert_eq!(result.postgres_user, postgresql_user);
+        assert_eq!(result.postgres_password, postgresql_password);
+        assert_eq!(result.postgres_db, postgresql_db);
+        assert_eq!(result.notion_token, notion_token);
+        assert_eq!(result.notion_dj_database_id, notion_dj_database_id);
+        assert_eq!(result.notion_dj_dashboard_h1_id, notion_dj_dashboard_h1_id);
+    }
+
+    #[test]
+    fn test_from_envar() {
+        let postgresql_url = "localhost";
+        let postgresql_port = "5432";
+        let postgresql_user = "user";
+        let postgresql_password = "hq812asd][;]";
+        let postgresql_db = "notion_dashboard";
+        let notion_token = "secret_1234567lkyqnsi_";
+        let notion_dj_database_id = "adjoas8d12hduayufg81g2312asd";
+        let notion_dj_dashboard_h1_id = "asdh1h298g9rwueh7139husfhew";
+
+        env::set_var("POSTGRES_URL", postgresql_url);
+        env::set_var("POSTGRES_PORT", postgresql_port);
+        env::set_var("POSTGRES_USER", postgresql_user);
+        env::set_var("POSTGRES_PASSWORD", postgresql_password);
+        env::set_var("POSTGRES_DB", postgresql_db);
+        env::set_var("NOTION_TOKEN", notion_token);
+        env::set_var("NOTION_DJ_DATABASE_ID", notion_dj_database_id);
+        env::set_var("NOTION_DJ_DASHBOARD_H1_ID", notion_dj_dashboard_h1_id);
+
+        let result = Config::from_envar();
+
+        assert_eq!(result.postgres_url, postgresql_url);
+        assert_eq!(result.postgres_port, postgresql_port);
+        assert_eq!(result.postgres_user, postgresql_user);
+        assert_eq!(result.postgres_password, postgresql_password);
+        assert_eq!(result.postgres_db, postgresql_db);
+        assert_eq!(result.notion_token, notion_token);
+        assert_eq!(result.notion_dj_database_id, notion_dj_database_id);
+        assert_eq!(result.notion_dj_dashboard_h1_id, notion_dj_dashboard_h1_id);
     }
 }
